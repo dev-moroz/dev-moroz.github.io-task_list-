d6962a2f10661bafd99867221b5d5ceb69610732
dev-moroz <dev_moroz@mail.ru>
2022-08-15 12:27:21 +0300

    fix: change deploy repositorie

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dev-moroz/task-list-sources.git master:gh-pages
+git push -f git@github.com:dev-moroz/task_list.git master:gh-pages
 
 cd -
